ead41506d214a74e22ce3123c54b26b42ef327c2
dilber37 <129645512+dilber37@users.noreply.github.com>
2023-04-15 17:58:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 const chatDisplay = document.getElementById('chat-display');
 const chatInput = document.getElementById('chat-input');
-const chatSend = document.getElementById('chat-send');
+const sendButton = document.getElementById('send-button');
 
-chatSend.addEventListener('click', sendMessage);
-chatInput.addEventListener('keypress', function(event) {
-  if (event.key === 'Enter') {
+sendButton.addEventListener('click', sendMessage);
+chatInput.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
     sendMessage();
   }
 });
 
 function sendMessage() {
-  const message = chatInput.value;
-  if (message) {
-    const newMessage = document.createElement('div');
-    newMessage.innerText = `You: ${message}`;
-    chatDisplay.appendChild(newMessage);
-    chatDisplay.scrollTop = chatDisplay.scrollHeight;
+  const message = chatInput.value.trim();
+  if (message !== '') {
+    appendMessage('You', message);
+    saveMessage('You', message);
     chatInput.value = '';
   }
 }
+
+function appendMessage(sender, content) {
+  const messageElement = document.createElement('div');
+  messageElement.innerHTML = `<strong>${sender}:</strong> ${content}`;
+  chatDisplay.appendChild(messageElement);
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
+}
+
+function saveMessage(sender, content) {
+  const messages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+  const newMessage = { sender, content };
+  messages.push(newMessage);
+  localStorage.setItem('chatMessages', JSON.stringify(messages));
+}
+
+function loadMessages() {
+  const messages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+  messages.forEach((message) => {
+    appendMessage(message.sender, message.content);
+  });
+}
+
+loadMessages();
+
